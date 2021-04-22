18a23fef531321300458abbcc5926f8420314c03
Johnny Howe <jonathon.o.howe@gmail.com>
2021-04-22 14:54:46 +1200

    Death on barrier collision

--- Assets/Car/Car_PlayerMovement.cs
+++ Assets/Car/Car_PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Car_Player : MonoBehaviour
+public class Car_PlayerMovement : MonoBehaviour
 {
     public bool accelerating = true;
     public float driveForce = 1;     // n
