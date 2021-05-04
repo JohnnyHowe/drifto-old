9351a13804f32946898f7bb915f8d57412444b8d
Johnny Howe <jonathon.o.howe@gmail.com>
2021-05-04 17:34:09 +1200

    Car aero drag
    Removed top speed (aero drag sorts this)

--- Assets/Car/Scripts/Car_PlayerMovement.cs
+++ Assets/Car/Scripts/Car_PlayerMovement.cs
@@ -6,11 +6,11 @@ using UnityEngine.Events;
 public class Car_PlayerMovement : MonoBehaviour
 {
     public bool accelerating = true;
-    public float driveForce = 1;     // n
-    public float maxSpeed = 10;             // m/s
+    public float drag = 1f;
+    public float driveForce = 1;
     public float maxAngularVelocity = 30;    // degrees per second
-    public float screenUse = 0.8f;  // How much of the screen to use for turning? max turn is when touch at screenUse of screen width
     public Transform centerOfMass;
+    public float screenUse = 0.8f;  // How much of the screen to use for turning? max turn is when touch at screenUse of screen width
     private Rigidbody _rb;
 
     void Start()
@@ -27,11 +27,16 @@ public class Car_PlayerMovement : MonoBehaviour
         {
             _rb.AddForce(_rb.transform.forward.normalized * driveForce);
         }
-        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, maxSpeed);
+        _rb.AddForce(-GetDragForce() * _rb.velocity.normalized);
 
         // Steering
         float steering = Mathf.Clamp(TouchInput.centeredScreenPosition.x / screenUse, -1, 1);
         _rb.angularVelocity = -Vector3.up * steering * maxAngularVelocity * Mathf.PI / 180;
         // _rb.angularVelocity = new Vector3(_rb.angularVelocity.x, -steering * maxAngularVelocity * Mathf.PI / 180, _rb.angularVelocity.z);
+    }
+
+    float GetDragForce()
+    {
+        return Mathf.Pow(_rb.velocity.magnitude, 2) * drag;
     }
 }
