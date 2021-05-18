a0adca069d347c9a01df2a544a0151172ca4ce3f
Johnny Howe <jonathon.o.howe@gmail.com>
2021-05-18 20:14:17 +1200

    Tyre smoke

--- Assets/Car/Scripts/Car_SkidMarks.cs
+++ Assets/Car/Scripts/Car_SkidMarks.cs
@@ -6,8 +6,10 @@ public class Car_SkidMarks : MonoBehaviour
 {
     public TrailRenderer[] tyreMarks;
     public TrailRenderer[] frontTyreMarks;
+    public ParticleSystem[] tyreSmoke;
     public Car_PlayerMovement car;
     public float width = 0.2f;
+    public float maxSmokeAlpha = 0.6f;
 
     void FixedUpdate() {
         Do();
@@ -18,6 +20,18 @@ public class Car_SkidMarks : MonoBehaviour
             t.startWidth = width;
             t.endWidth = width;
             t.emitting = car.IsDrifting();
+        }
+        foreach(ParticleSystem ps in tyreSmoke) {
+            if (car.IsDrifting()) {
+                ps.Play();
+                Material mat = ps.gameObject.GetComponent<ParticleSystemRenderer>().material;
+                Color color = mat.color;
+                float a = (car.GetDriftAngle() / 90) * maxSmokeAlpha;
+                color.a = Mathf.Min(a * a, maxSmokeAlpha);
+                mat.color = color;
+            } else {
+                ps.Stop();
+            }
         }
         foreach (TrailRenderer t in frontTyreMarks) {
             t.startWidth = width;
