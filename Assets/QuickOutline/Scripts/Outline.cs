1ab98980e456c58b7605b08adb35adf03375459c
Johnny Howe <jonathon.o.howe@gmail.com>
2021-05-21 09:13:54 +1200

    fjhaksdhfldsf

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -59,7 +59,7 @@ public class Outline : MonoBehaviour {
   [SerializeField]
   private Color outlineColor = Color.white;
 
-  [SerializeField, Range(0f, 10f)]
+  [SerializeField, Range(0f, 50f)]
   private float outlineWidth = 2f;
 
   [Header("Optional")]
